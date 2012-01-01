99b47aaed0c374f4afb325358238ecabfdad3843
Jake Luer <jake.luer@gmail.com>
2012-01-01 15:11:54 -0500

    cleaned up examples

--- examples/console.js
+++ examples/console.js
@@ -8,12 +8,12 @@ var log = new tea.Logger({
     ]
 });
 
-log.log('Hello World');
+log.log('info', 'Hello World');
 log.info('This is info.');
 log.debug('This is debug');
 log.notice('this is a notice');
 log.warning('this is a warning');
 log.error('this is an error');
-log.crit('This is critical!!');
+log.critical('This is critical!!');
 log.alert('this is an alert');
-log.emerg('this is an emergency');
+log.emergency('this is an emergency');
