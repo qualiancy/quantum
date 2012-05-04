65f30d4670b54009fc447a9b79139e4845df4a92
Jake Luer <jake.luer@gmail.com>
2012-05-03 22:57:50 -0400

    console examples in one file

--- examples/console.js
+++ examples/console.js
@@ -1,15 +1,27 @@
-var tea = require('..')
-  , log = new tea.Logger('themed');
+var tea = require('..');
 
-log.use(tea.console({ theme: 'npm' }));
-log.init();
 
-log.log('info', 'Tea please!');
-log.info('This is info.');
-log.debug('This is debug.');
-log.notice('This is a notice.');
-log.warn('This is a warning.');
-log.error('This is an error.');
-log.crit('This is critical!!');
-log.alert('This is an alert.');
-log.emerg('This is an emergency.');
+console.log('');
+
+[ 'clean', 'default', 'npm' ].forEach(function (theme) {
+  console.log('== THEME: %s ==', theme);
+  console.log('');
+
+  var log = new tea.Logger('my-app');
+
+  log.use(tea.console({ theme: theme }));
+  log.init();
+
+  log.log('info', 'Tea please!');
+  log.info('This is info.');
+  log.debug('This is debug.');
+  log.notice('This is a notice.');
+  log.warn('This is a warning.');
+  log.error('This is an error.');
+  log.crit('This is critical!!');
+  log.alert('This is an alert.');
+  log.emerg('This is an emergency.');
+
+  console.log('');
+  console.log('');
+});
