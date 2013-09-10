866a44a9bb538c5dd1d6aab2530eddb40d560a08
Jake Luer <jake@alogicalparadox.com>
2013-09-10 12:41:32 -0400

    refactor logger constructor function
    - add tests for configure
    - add tests for construction
    - test organization

--- test/streams.js
+++ test/streams.js
@@ -1,48 +1,117 @@
-describe.only('Logger', function () {
-  it('should write to writable streams', function (done) {
-    var logger = new quantum.Logger('test-namespace');
-    var stream = new MockWritable();
-    var testEvent = { type: 'debug', msg: 'Test message' };
+describe.only('Logger', function() {
+  var Logger = quantum.Logger;
 
-    logger.pipe(stream);
+  function build(ns, opts) {
+    ns = ns || 'test-namespace';
+    return new Logger(ns, opts);
+  }
 
-    stream.on('_write', function (ev) {
-      ev.should.deep.equal(testEvent);
-      done();
+  describe('when constructed', function() {
+    it('sets env setting', function() {
+      var logger = build();
+      var ENV = process.env.NODE_ENV || 'development';
+      logger.get('env').should.equal(ENV);
     });
 
-    logger.write(testEvent);
+    it('sets default tokens', function() {
+      var logger = build();
+      var def = [ 'process', 'memory' ];
+      logger.get('tokens').should.deep.equal(def);
+    });
+
+    it('mounts syslog levels');
   });
 
-  it('should read from readable streams', function (done) {
-    var logger = new quantum.Logger('test-namespace');
-    var stream = new MockReadable();
-    var testEvent = { type: 'info', msg: 'Test message' };
-
-    stream.pipe(logger);
-
-    logger.on('readable', function() {
-      var ev = this.read();
-      ev.should.eql(testEvent);
-      done();
+  describe('when configured', function() {
+    it('invokes non-env function', function() {
+      var logger = build();
+      var spy = chai.spy('non-env');
+      logger.configure(spy);
+      spy.should.have.been.called(1);
     });
 
-    stream._queue.push(testEvent);
-    stream.emit('_ready');
+    it('invokes current env function', function() {
+      var logger = build();
+      var spy = chai.spy('env');
+      var env = logger.get('env');
+      logger.configure(env, spy);
+      spy.should.have.been.called(1);
+    });
+
+    it('ignores non-current env function', function() {
+      var logger = build();
+      var spy = chai.spy('non-current');
+      var env = logger.get('env') + '-not';
+      logger.configure(env, spy);
+      spy.should.have.not.been.called();
+    });
+
+    it('invokes when more than one env used', function() {
+      var logger = build();
+      var spy = chai.spy('more-than-one');
+      var env = logger.get('env');
+      logger.configure(env + '-not', env, spy);
+      spy.should.have.been.called(1);
+    });
   });
 
-  it('should write to a writable stream from a mounted level', function (done) {
-    var logger = new quantum.Logger('test-namespace');
-    var stream = new MockWritable();
+  describe('when written to', function() {
+    describe('with #write', function() {
+      it('pipes to writable streams', function(done) {
+        var logger = build();
+        var stream = new MockWritable();
+        var testEvent = { type: 'debug', msg: 'Test message' };
 
-    logger.pipe(stream);
+        logger.pipe(stream);
 
-    stream.on('_write', function (ev) {
-      ev.should.have.property('type', 'info');
-      ev.should.have.property('msg', 'Test message');
-      done();
+        stream.on('_write', function(ev) {
+          ev.should.deep.equal(testEvent);
+          done();
+        });
+
+        logger.write(testEvent);
+      });
     });
 
-    logger.log('info', 'Test message');
+    describe('with mounted level', function() {
+      it('pipes to writable streams', function(done) {
+        var logger = build();
+        var stream = new MockWritable();
+
+        logger.pipe(stream);
+
+        stream.on('_write', function(ev) {
+          ev.should.have.property('type', 'info');
+          ev.should.have.property('msg', 'Test message');
+          done();
+        });
+
+        logger.log('info', 'Test message');
+      });
+    });
+  });
+
+  describe('when read from', function() {
+    describe('via readable stream', function() {
+      it('emits readable event', function(done) {
+        var logger = build();
+        var stream = new MockReadable();
+        var testEvent = { type: 'info', msg: 'Test message' };
+
+        stream.pipe(logger);
+
+        logger.on('readable', function() {
+          var ev = this.read();
+          ev.should.eql(testEvent);
+          done();
+        });
+
+        stream._queue.push(testEvent);
+        stream.emit('_ready');
+      });
+
+      it('keeps original namespace');
+      it('filters malformed leg events');
+    });
   });
 });
