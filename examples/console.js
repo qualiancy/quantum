2366c1f58d61855080f2baf00953dfe1e272b612
Jake Luer <jake@alogicalparadox.com>
2013-11-10 09:58:32 -0500

    stream: [stdout/stderr] add and update example

--- examples/console.js
+++ examples/console.js
@@ -1,7 +1,7 @@
 var quantum = require('..');
 var log = quantum('console');
 
-log.pipe('theme', { theme: 'default' }).pipe(process.stdout);
+log.pipe('stdout', { theme: 'json' });
 
 log.debug('hello debug');
 log.info('hello info');
